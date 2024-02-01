08d67e8d5a1a4633462de8aa0dd68e0ef3c77652
JorgeDCh20 <JorgeDCh20>
2024-01-31 23:25:05 -0500

    adding propeties

--- src/main/java/com/horn/blue/BlueApplication.java
+++ src/main/java/com/horn/blue/BlueApplication.java
@@ -2,8 +2,10 @@ package com.horn.blue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.horn.blue.entities")
 public class BlueApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/horn/blue/entities/PhotoProfile.java
+++ src/main/java/com/horn/blue/entities/PhotoProfile.java
@@ -3,19 +3,17 @@ package com.horn.blue.entities;
 import lombok.Data;
 
 import javax.persistence.*;
-
 @Data
 @Entity
 @Table (name = "PhotoProfile")
-
 public class PhotoProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String content;
+
     @Column(name = "photourl", nullable = true)
     private String photourl;
-    //ol
 }
 
