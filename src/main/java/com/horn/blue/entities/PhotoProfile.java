9e3455b9899560d0afcac177774eb8154ee779af
JorgeDCh20 <JorgeDCh20>
2024-01-31 22:39:43 -0500

    adding propeties

---
+++ src/main/java/com/horn/blue/entities/PhotoProfile.java
@@ -0,0 +1,20 @@
+package com.horn.blue.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table (name = "PhotoProfile")
+
+public class PhotoProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    @Column(name = "photourl", nullable = true)
+    private String photourl;
+}
+
